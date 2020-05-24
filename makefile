d2936551f0fbb8993d03eaed251815f6b04aeac3
Jason Nguyen <jnguyen1098@github.com>
2020-05-24 04:32:35 -0400

    basic project structure

---
+++ makefile
@@ -0,0 +1,17 @@
+CC = clang
+CFLAGS = -Wall -Wpedantic -Wextra -ggdb
+VFLAGS = --leak-check=full --show-leak-kinds=all --track-origins=yes
+
+.PHONY = run clean
+
+run: stash
+	./stash
+
+valgrind: stash
+	valgrind $(VFLAGS) ./stash
+
+stash: stash.c
+	$(CC) $(CFLAGS) stash.c -o stash
+
+clean:
+	rm -rf stash
--- stash.c
+++ stash.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 
-int main(void)
+void write_vlv(unsigned long value, char vlv[5])
+{
+    return;
+}
+
+unsigned long read_vlv(char vlv[5])
 {
     return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    char arr[5] = {81, 81, 81, 81, 1};
+    printf("%lu\n", read_vlv(arr));
+
+    if (argc == 1)
+        return fprintf(stderr, "Usage: %s midifile [binaryfile]\n", argv[0]), 1;
+
+    // decrypt
+    if (argc == 2) {
+        FILE *midifile;
+        if (!(midifile = fopen(argv[1], "r")))
+            return fprintf(stderr, "File error\n"), 2;
+
+        // 
+
+        return 0;
+    }
+
+    // encrypt
+    else if (argc == 3) {
+        FILE *midifile, *binaryfile;
+        if (!(midifile = fopen(argv[1], "r+")) || !(binaryfile = fopen(argv[2], "r")))
+            return fprintf(stderr, "File error\n"), 2;
+
+        // 
+
+        return 0;
+    }
+
+    return fprintf(stderr, "Usage: %s midifile [binaryfile]\n", argv[0]);
 }
