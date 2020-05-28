3f86bb20eaf26ffbc533541de76d6fa09bd33b45
jnguyen1098 <jnguyen1098@gmail.com>
2020-05-27 21:00:37 -0400

    read_vlv

--- stash.c
+++ stash.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 
-void write_vlv(unsigned long value, char vlv[5])
+void write_vlv(unsigned long value, unsigned char vlv[5])
 {
+    for (int i = 31; i >= 0; i--)
+        printf("%lu", value >> i & 1);
+    puts("");
+    for (int i = 0; i < 32; i++) {
+        unsigned long val = value >> i & 1;
+
+    }
+    int top = 0;
+
+    puts("");
     return;
 }
 
-unsigned long read_vlv(char vlv[5])
+unsigned long read_vlv(unsigned char vlv[5])
 {
-    return 0;
+    unsigned long result = 0;
+    for (int i = 0; i < 5; i++)
+        result += vlv[i] & 0x7F;
+    return result;
 }
 
 int main(int argc, char *argv[])
 {
-    char arr[5] = {81, 81, 81, 81, 1};
+    unsigned char arr[5] = {0xff, 0x91, 0x81, 0x83, 0x1};
     printf("%lu\n", read_vlv(arr));
+
+    unsigned char vlv[5] = { 0 };
+    write_vlv(4294967295, vlv);
+    for (int i = 0; i < 5; i++)
+        printf("%d ", vlv[i]);
+    puts("");
 
     if (argc == 1)
         return fprintf(stderr, "Usage: %s midifile [binaryfile]\n", argv[0]), 1;
