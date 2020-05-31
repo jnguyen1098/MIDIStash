cc6d4af4e9d57de61e330f7aa697541cf0acfc96
jnguyen1098 <jnguyen1098@gmail.com>
2020-05-30 23:30:40 -0400

    unstable build, experimenting with file writing. do not use

--- stash.c
+++ stash.c
@@ -36,6 +36,48 @@ unsigned long read_vlv(unsigned char vlv[5])
     return value;
 }
 
+int operate_midi(FILE *midifile, FILE *binaryfile)
+{
+    // ptr sanity
+    if (!midifile)
+        return 0;
+
+    // MThd 0x00 0x00 0x00 0x06
+    if (fgetc(midifile) != 0x4D || fgetc(midifile) != 0x54 ||
+        fgetc(midifile) != 0x68 || fgetc(midifile) != 0x64 ||
+        fgetc(midifile) != 0x00 || fgetc(midifile) != 0x00 ||
+        fgetc(midifile) != 0x00 || fgetc(midifile) != 0x06)
+        return 0;
+
+    // Type-1 MIDI
+    if (fgetc(midifile) != 0x00 || fgetc(midifile) != 0x01)
+        return 0;
+
+    // Track count
+    size_t trkcnt = (fgetc(midifile) << 8) + fgetc(midifile) + 1;
+    fseek(midifile, -2, SEEK_CUR);
+    fputc((unsigned char)((trkcnt >> 8) % 256), midifile);
+    fputc((unsigned char)((trkcnt % 256)), midifile);
+
+    fseek(midifile, 0, SEEK_END);
+
+    char str[] = {'M', 'T', 'r', 'k', '\0'};
+    fputs(str, midifile);
+
+    unsigned char vlv[5] = { 0 };
+    fseek(binaryfile, 0, SEEK_END);
+    unsigned long binaryfile_size = ftell(binaryfile);
+    size_t num_bytes = write_vlv(binaryfile_size, vlv);
+    rewind(binaryfile);
+    for (size_t i = 0; i < num_bytes; i++) {
+        printf("byte: %x\n", vlv[i]);
+        fputc((char)vlv[i], midifile);
+    }
+
+    rewind(midifile);
+    return 1;
+}
+
 int main(int argc, char *argv[])
 {
     if (argc == 1)
@@ -49,6 +91,7 @@ int main(int argc, char *argv[])
 
         // 
 
+        fclose(midifile);
         return 0;
     }
 
@@ -58,8 +101,11 @@ int main(int argc, char *argv[])
         if (!(midifile = fopen(argv[1], "r+")) || !(binaryfile = fopen(argv[2], "r")))
             return fprintf(stderr, "File error\n"), 2;
 
-        // 
+        if (!operate_midi(midifile, binaryfile))
+            return fprintf(stderr, "Invalid MIDI\n"), 3;
 
+        fclose(midifile);
+        fclose(binaryfile);
         return 0;
     }
 
